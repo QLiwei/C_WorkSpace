97c548920a197321b3976a013a140c8936ff8d7a
vector.qiu <vector.qiu@anker-in.com>
2023-01-03 20:10:11 +0800

    type:feat scope: subject:file operation

--- 12_file_input_output.c
+++ 12_file_input_output.c
@@ -16,6 +16,7 @@
 #include <string.h>
 /* Private typedef -----------------------------------------------------------*/
 /* Private define ------------------------------------------------------------*/
+#define BUFSIZE 4096
 /* Private macro -------------------------------------------------------------*/
 /* Private variables ---------------------------------------------------------*/
 /* Private function prototypes -----------------------------------------------*/
@@ -161,7 +162,7 @@ int main(int argc, char *argv[]) {
  * @brief fgetpos() fsetpos()
  *
  */
-#if 1
+#if 0
     #define BUFSIZE 4096
     #define SLEN    81
 
@@ -213,6 +214,44 @@ int main(int argc, char *argv[]) {
     }
     puts("Done displaying.");
     fclose(fa);
+
+#endif
+
+#if 1
+    #define ARSIZE 1000
+
+    double value;
+    double numbers[ARSIZE];
+    const char *file = "number.dat";
+    int i;
+    long pos;
+    FILE *iofile;
+
+    for (i = 0; i < ARSIZE; i++) {
+        numbers[i] = 100.0 * i + 1.0 / (i + 1);
+    }
+
+    if ((iofile = fopen(file, "wb")) == NULL) {
+        fprintf(stderr, "Could not open %s for output.\n", file);
+        exit(EXIT_FAILURE);
+    }
+    fwrite(numbers, sizeof(double), ARSIZE, iofile);
+    fclose(iofile);
+
+    if ((iofile = fopen(file, "rb")) == NULL) {
+        fprintf(stderr, "Could not open %s for random access.\n", file);
+        exit(EXIT_FAILURE);
+    }
+    printf("Enter an index in the range 0-%d.\n", ARSIZE - 1);
+    while (scanf("%d", &i) == 1 && i >= 0 && i < ARSIZE) {
+        pos = (long)i * sizeof(double);
+        fseek(iofile, pos, SEEK_SET);
+        fread(&value, sizeof(double), 1, iofile);
+        printf("The value there is %f.\n", value);
+        printf("Next index (out of range to quit):\n");
+    }
+    fclose(iofile);
+    puts("Bye!");
 
 #endif
     return 0;
